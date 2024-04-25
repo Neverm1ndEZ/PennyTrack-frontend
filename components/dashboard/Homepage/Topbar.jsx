e800c6e48e1842106a94e323225a4ab74c2069b5
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-04-25 13:17:11 +0530

    minor-frontend-changes

--- components/dashboard/Homepage/Topbar.jsx
+++ components/dashboard/Homepage/Topbar.jsx
@@ -4,28 +4,30 @@ import { Avatar } from "@material-tailwind/react";
 import axios from "axios";
 
 export default function Topbar() {
-  const [fullName, setFullName] = useState("");
+	const [fullName, setFullName] = useState("");
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const fullName = localStorage.getItem("fullName");
-      setFullName(fullName);
-    }
-  }, []);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const fullName = localStorage.getItem("fullName");
+			setFullName(fullName);
+		}
+	}, []);
 
-  return (
-    <div className="flex justify-between items-center p-8 h-28 w-full text-light-green-500">
-      <h1 className="text-3xl font-medium">Welcome to PennyTrack Dashboard</h1>
-      <div className="w-[16rem] bg-[#1d1d1d] px-4 py-2.5 rounded-lg">
-        <div className="flex gap-x-6 items-center">
-          <Avatar
-            src="https://docs.material-tailwind.com/img/face-2.jpg"
-            alt="avatar"
-            size="lg"
-          />
-          {fullName}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex justify-between items-center pl-8 py-8 h-28 w-full text-light-green-500">
+			<h1 className="text-3xl font-medium mr-20">
+				Welcome to PennyTrack Dashboard
+			</h1>
+			<div className="w-[15%] bg-[#1d1d1d] ml-10 px-4 py-2.5 rounded-lg">
+				<div className="flex gap-x-6 items-center">
+					<Avatar
+						src="https://docs.material-tailwind.com/img/face-2.jpg"
+						alt="avatar"
+						size="lg"
+					/>
+					{fullName}
+				</div>
+			</div>
+		</div>
+	);
 }
